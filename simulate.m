aed5c6afc18b9aeba314a0d8a2f032c64d832e18
xgdgsc <xgdgsc@gmail.com>
2014-01-07 21:39:21 +0800

    simulate AR time series generating routine

---
+++ simulate.m
@@ -0,0 +1,9 @@
+model1 = arima('Constant',0,'AR',0.3,'ARLags',1,'Variance',0.01);
+[Y1,E]=simulate(model,1000);
+figure(1);
+plot(Y1);
+
+model2 = arima('Constant',0,'AR',0.6,'ARLags',1,'Variance',0.01);
+[Y2,E]=simulate(model,1000);
+figure(2);
+plot(Y2);
