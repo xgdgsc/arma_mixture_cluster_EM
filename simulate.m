c273d14b90069461ebe78f60697211e8ac9ee4d6
xgdgsc <xgdgsc@gmail.com>
2014-01-08 15:03:14 +0800

    added comment

--- simulate.m
+++ simulate.m
@@ -1,9 +1,12 @@
-model1 = arima('Constant',0,'AR',0.3,'ARLags',1,'Variance',0.01);
-[Y1,E]=simulate(model,1000);
-figure(1);
-plot(Y1);
+%% AR generating example, TODO: write function wrapper to generate all the required time series
+% model1 = arima('Constant',0,'AR',0.3,'ARLags',1,'Variance',0.01);
+% [Y1,E]=simulate(model,1000);
+% figure(1);
+% plot(Y1);
+% 
+% model2 = arima('Constant',0,'AR',0.6,'ARLags',1,'Variance',0.01);
+% [Y2,E]=simulate(model,1000);
+% figure(2);
+% plot(Y2);
 
-model2 = arima('Constant',0,'AR',0.6,'ARLags',1,'Variance',0.01);
-[Y2,E]=simulate(model,1000);
-figure(2);
-plot(Y2);
+%% Simulate Process
