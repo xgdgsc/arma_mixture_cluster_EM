2aedef3c12b05e929d0ec147fd162f7927cb7ce1
xgdgsc <xgdgsc@gmail.com>
2014-01-08 20:37:42 +0800

    parameter class to hold parameter of one ARMA series

---
+++ parameter.m
@@ -0,0 +1,17 @@
+%% parameter class to contain ARMA parameters (for one cluster)
+classdef parameter 
+    
+    properties
+        %class properties
+        phi_array;
+        theta_array;
+        sigma;
+        
+    end
+    
+    methods
+        function random_init(self)
+        end
+    end
+    
+end
